097a5aafaf548f5e572860ddd25e5a9fca6485a1
Abdullah Zeeshan <4bdullah7eeshan@proton.me>
2024-11-12 18:55:47 +0530

    feat: setup signin and signup pages

---
+++ scripts/signup.js
@@ -0,0 +1,38 @@
+// Sign Up
+
+const signupForm = document.getElementById("signupForm");
+
+signupForm.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const data = {
+        username,
+        password
+    };
+
+    try {
+        const response = await fetch("http://localhost:3000/auth/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            alert("Sign-up successful! Please log in.");
+            window.location.href = "./signin.html";
+        } else {
+            const errorData = await response.json();
+            alert(`Error: ${errorData.message}`);
+        }
+    } catch (error) {
+        alert("An error occurred. Please try again later.");
+    }
+
+    
+});
